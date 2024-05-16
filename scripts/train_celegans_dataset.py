82637f896efcca81826041a26e152edf170febc9
Aleksandra Pidde <aleksandra.pidde@gmail.com>
2024-05-16 09:49:07 +0200

    fix generation of datasets, network does not train

--- scripts/train_celegans_dataset.py
+++ scripts/train_celegans_dataset.py
@@ -25,13 +25,14 @@ def celegans_config(zarrStore):
         "raw_data_path": zarrStore,
         "csv_path": "/group/dl4miacourse/projects/OptiMates/TRNs_calcium/batch1/tracks/GN692_125kPa002_20231017-142851.csv",
         "ndims": 3,
-        "voxel_size": (1, 1, 1)
+        "voxel_size": (1, 1, 1),
+        "dtype": np.uint16
     }
 
 def get_model():
     # these are default values
     # You will need to change them for your training
-    return UNet(
+    unet = UNet(
             in_channels=1,
             num_fmaps=16,
             fmap_inc_factor=2,
@@ -41,6 +42,9 @@ def get_model():
             num_heads=1,
             constant_upsample=True,
             padding='same')
+    conv = torch.nn.Conv3d(16,1,1)
+    sigm = torch.nn.Sigmoid()
+    return torch.nn.Sequential(unet, conv, sigm)
 
 
 if __name__ == "__main__":
@@ -56,26 +60,37 @@ if __name__ == "__main__":
     input_size =(3, 256, 256)
     output_size = (3, 256, 256)
 
-    pipeline = get_pipeline(data_config, model, torch.nn.MSELoss(), optimizer, input_size, output_size, radius=(0,5,5), augment_only=True)
+    pipeline, request = get_pipeline(data_config, model, torch.nn.MSELoss(), optimizer, input_size, output_size, radius=(0,5,5))
 
 
 
     raw_key = gp.ArrayKey("RAW")
     points_key = gp.GraphKey("POINTS")
     cell_indicator = gp.ArrayKey('CELL_INDICATOR')
-    request = gp.BatchRequest()
-    request.add(raw_key, gp.Coordinate(input_size))
-    request.add(points_key, gp.Coordinate(input_size))
-    request.add(cell_indicator, gp.Coordinate(input_size))
-    with gp.build(pipeline):
-        root = zarr.open("test.zarr", 'w')
-        for i in range(10):
-            print(i)
+    pred_cell_indicator = gp.ArrayKey('PRED_CELL_INDICATOR')
+    debug_request = gp.BatchRequest()
+    debug_request.add(raw_key, gp.Coordinate(input_size))
+    debug_request.add(points_key, gp.Coordinate(input_size))
+    debug_request.add(cell_indicator, gp.Coordinate(input_size))
+    debug_request.add(pred_cell_indicator, gp.Coordinate(output_size))
 
-            batch = pipeline.request_batch(request)
-            iteration = root.create_group(i)
-            iteration["raw"] = batch[raw_key].data
-            iteration["cell_indicator"] = batch[cell_indicator].data
+    debug = False
+    with gp.build(pipeline):
+        for i in range(3):
+            print(i)
+            if debug:
+                root = zarr.open("test_training.zarr", 'w')
+                batch = pipeline.request_batch(debug_request)
+                iteration = root.create_group(i)
+                iteration["raw"] = batch[raw_key].data
+                iteration["cell_indicator"] = batch[cell_indicator].data
+                iteration["pred_cell_indicator"] = batch[pred_cell_indicator].data
+            else:
+                max_iterations = 200000  # you will definitely want more iterations
+                print("Starting training...")
+                for i in range(max_iterations):
+                    pipeline.request_batch(request)
+
         
     # run_training(data_config, model)
 
